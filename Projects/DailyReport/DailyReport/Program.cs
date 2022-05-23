a957c35e24a4a91d0cfccf522e0b48f766747614
Jeremy Sonnenschein <100240007+JeremySunshine@users.noreply.github.com>
2022-05-23 12:34:18 -0400

    Daily Report Assignment

---
+++ Projects/DailyReport/DailyReport/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace DailyReport
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("The Tech Academy!");
+            Console.ReadLine();
+
+
+
+
+            Console.WriteLine("Student Daily Report");
+            Console.ReadLine();
+
+
+
+
+            Console.WriteLine("What is your name?");
+            string yourName = Console.ReadLine();
+
+
+
+
+            Console.WriteLine("Hello: " + yourName + " What course are you on?");
+            string CourseName = Console.ReadLine();
+
+
+
+
+
+
+            Console.WriteLine("What Page on " + CourseName);
+            Console.ReadLine();
+
+
+
+
+
+
+
+            Console.WriteLine("Do you need help with anything. Please answer true or false");
+            Console.ReadLine();
+
+
+
+            Console.WriteLine("Where there any posotive experiences you would like to share? Please give specifics.");
+            Console.ReadLine();
+
+
+
+
+
+
+
+            Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
+            Console.ReadLine();
+
+
+
+
+
+            Console.WriteLine("How many hours did you study today?");
+            Console.ReadLine();
+
+            Console.WriteLine("Thank your for your answers. An instructor will espond to this shortly, have a great day!");
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
