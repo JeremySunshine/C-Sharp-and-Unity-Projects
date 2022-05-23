410721832803e0851dc4e4b325b92dee8c74d9c2
Jeremy Sonnenschein <100240007+JeremySunshine@users.noreply.github.com>
2022-05-23 15:11:15 -0400

    Boolean Logic Assignment

---
+++ Projects/BooleanLogicAssignment/BooleanLogicAssignment/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogicAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            string yourAge = Console.ReadLine();
+            int age = 15;
+            Console.WriteLine("you are " + yourAge + " years of age");
+            
+
+
+            Console.WriteLine("Have you ever had a DUI?");
+            string yourDUI = Console.ReadLine();
+            Console.WriteLine();
+
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            string yourTickets = Console.ReadLine();
+
+            bool isQaulify = (yourAge == "15" && yourDUI == "false" && yourTickets == "3");
+
+
+
+
+            
+
+
+
+        }
+    }
+}
