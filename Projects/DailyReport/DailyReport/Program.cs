f6e3cb5973939588a37b12b9e9259f53d20d0fca
Jeremy Sonnenschein <100240007+JeremySunshine@users.noreply.github.com>
2022-05-24 13:53:36 -0400

    fixed errors

--- Projects/DailyReport/DailyReport/Program.cs
+++ Projects/DailyReport/DailyReport/Program.cs
@@ -9,68 +9,45 @@ namespace DailyReport
             Console.WriteLine("The Tech Academy!");
             Console.ReadLine();
 
-
-
-
             Console.WriteLine("Student Daily Report");
             Console.ReadLine();
-
-
 
 
             Console.WriteLine("What is your name?");
             string yourName = Console.ReadLine();
 
 
-
-
             Console.WriteLine("Hello: " + yourName + " What course are you on?");
             string CourseName = Console.ReadLine();
 
 
-
-
-
-
             Console.WriteLine("What Page on " + CourseName);
-            Console.ReadLine();
-
-
-
-
-
+            string pageNum = Console.ReadLine();
+            int pageNumInt = Convert.ToInt32(pageNum);
 
 
             Console.WriteLine("Do you need help with anything. Please answer true or false");
-            Console.ReadLine();
+            string needHelp = Console.ReadLine();
+            bool needHelp1 = Convert.ToBoolean(needHelp);
 
 
 
-            Console.WriteLine("Where there any posotive experiences you would like to share? Please give specifics.");
-            Console.ReadLine();
-
-
-
-
+            Console.WriteLine("Where there any positive experiences you would like to share? Please give specifics.");
+            string posEx = Console.ReadLine();
 
 
 
             Console.WriteLine("Is there any other feedback you'd like to provide? Please be specific.");
-            Console.ReadLine();
-
-
-
-
+            string feedBack = Console.ReadLine();
+           
 
             Console.WriteLine("How many hours did you study today?");
-            Console.ReadLine();
+            string hoursStudy = Console.ReadLine();
+            int hoursStudy1 = Convert.ToInt32(hoursStudy);
+            
 
             Console.WriteLine("Thank your for your answers. An instructor will espond to this shortly, have a great day!");
             Console.ReadLine();
-
-
-
-
 
         }
     }
