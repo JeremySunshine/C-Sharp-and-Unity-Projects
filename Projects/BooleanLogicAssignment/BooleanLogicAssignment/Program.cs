aadb48edb00ecf1ecea3d132c6bd549164f97f78
Jeremy Sonnenschein <100240007+JeremySunshine@users.noreply.github.com>
2022-05-26 09:57:00 -0400

    fixed errors

--- Projects/BooleanLogicAssignment/BooleanLogicAssignment/Program.cs
+++ Projects/BooleanLogicAssignment/BooleanLogicAssignment/Program.cs
@@ -12,25 +12,30 @@ namespace BooleanLogicAssignment
         {
             Console.WriteLine("What is your age?");
             string yourAge = Console.ReadLine();
-            int age = 15;
+            int ageINT = Convert.ToInt32(yourAge);
             Console.WriteLine("you are " + yourAge + " years of age");
             
 
 
-            Console.WriteLine("Have you ever had a DUI?");
-            string yourDUI = Console.ReadLine();
-            Console.WriteLine();
+            Console.WriteLine("Have you ever had a DUI? Please answer true or false.");
+            string DUI = Console.ReadLine();
+            bool dui1 = Convert.ToBoolean(DUI);
 
 
             Console.WriteLine("How many speeding tickets do you have?");
             string yourTickets = Console.ReadLine();
+            int ticketsINT = Convert.ToInt32(yourTickets);
 
-            bool isQaulify = (yourAge == "15" && yourDUI == "false" && yourTickets == "3");
+            const string V = "15";
+            bool isQuailified = ageINT <= V && DUI == "false" && yourTickets <= "3";
 
 
 
 
-            
+
+
+
+
 
 
 
