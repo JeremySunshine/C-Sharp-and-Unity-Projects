faac1f2d5f20f86e56785ecdb1fce54ad1867b3e
Jeremy Sonnenschein <100240007+JeremySunshine@users.noreply.github.com>
2022-06-02 13:36:29 -0400

    tryCatch

---
+++ Projects/tryCatchAssignment/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tryCatchAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> intList = new List<int>();
+            intList.Add(10);
+            intList.Add(12);
+            intList.Add(14);
+            intList.Add(16);
+            intList.Add(18);
+            intList.Add(20);
+            
+
+
+            Console.WriteLine("Select a number you would like to divide by");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            for ()
+            {
+
+            }
+
+            
+
+
+
+
+
+        }
+    }
+}
