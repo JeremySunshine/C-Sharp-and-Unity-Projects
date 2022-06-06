cdce3151edc9f9c4d70d115c263c7504734daa0d
Jeremy Sonnenschein <100240007+JeremySunshine@users.noreply.github.com>
2022-06-06 15:26:09 -0400

    CallingMethodsAssignment

---
+++ Projects/callingMethods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace callingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mathOpp mathObject = new mathOpp();
+            Console.WriteLine("What number would you like to do math operations on?");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(mathObject.method1(userInput));
+            Console.WriteLine(mathObject.method2(userInput));
+            Console.WriteLine(mathObject.method3(userInput));
+
+
+
+        }
+        
+
+       
+    }
+}
---
+++ Projects/callingMethods/mathOpp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace callingMethods
+{
+    public class mathOpp
+    {
+    
+        public int method1(int number)
+        {
+            return number + 5;
+        }
+
+        public int method2(int number)
+        {
+            return number - 5;
+        }
+
+        public int method3(int number)
+        {
+            return number * 5;
+        }
+    }
+}
