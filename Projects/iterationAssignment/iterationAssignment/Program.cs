1f820b2c9d39f40ef4901128a281f3f0d7b70f31
Jeremy Sonnenschein <100240007+JeremySunshine@users.noreply.github.com>
2022-06-06 14:45:39 -0400

    fixed errors

--- Projects/iterationAssignment/iterationAssignment/Program.cs
+++ Projects/iterationAssignment/iterationAssignment/Program.cs
@@ -10,49 +10,56 @@ namespace iterationAssignment
     {
         static void Main(string[] args)
         {
-            //one dimensional array
+            //Part 1
             String[] cars = { "BMW", "Mustang", "Corvette" };
+            Console.WriteLine("Enter some text to appened to my car names");
+            
 
-            Console.WriteLine(cars[0]);
-            Console.WriteLine(cars[1]);
-            Console.WriteLine(cars[2]);
 
-            Console.ReadLine();
+            string userInput = Console.ReadLine();
+            for (int i = 0; i < cars.Length; i++)
+            {
+                cars[i] = cars[i] + userInput;
+            }
+            for (int i = 0; i < cars.Length; i++)
+            {
+                Console.WriteLine(cars[i]);
+            }
 
             
-            Console.WriteLine("Hello! What is your name?");
-            string name = Console.ReadLine();
-            Console.WriteLine("Hello " + name + " I hope your day is going well");
+            
             Console.ReadLine();
 
             Console.WriteLine("Enter you age: ");
             string age = Console.ReadLine();
 
-            while (age == "")
+            // Part 2 fixed infinite loop
+            while (age == "") 
             {
                 Console.Write("Enter Your age: ");
                 age = Console.ReadLine();
             }
 
             int[] testScores = { 85, 81, 69, 91, 100, 77, 85, 73 };
-
+            // Part 3 < 
             for (int i = 0; i < testScores.Length; i++)
             {
-                if (testScores[i] > 80)
-                {
+                
+                
                     Console.WriteLine("Passing test Scores: " + testScores[i]);
-                }
+                
             }
             Console.ReadLine();
 
+            // Part 3 <=
             int[] testScores2 = { 85, 81, 69, 91, 100, 77, 85, 73 };
 
-            for (int f = 0; f < testScores2.Length; f++)
+            for (int f = 0; f <= testScores2.Length - 1; f++)
             {
-                if (testScores2[f] < 80)
-                {
+                
+                
                     Console.WriteLine("Failing test Scores: " + testScores2[f]);
-                }
+                
             }
 
             Console.ReadLine();
@@ -60,7 +67,7 @@ namespace iterationAssignment
 
             Console.WriteLine("See if your number is on the list ");
             
-
+            // Part 4 
             List<string> number = new List<string>();
             number.Add("1");
             number.Add("2");
@@ -74,32 +81,62 @@ namespace iterationAssignment
             number.Add("10");
             
 
-            int favNum = Convert.ToInt32(Console.ReadLine());
+            string favNum = Console.ReadLine();
             
 
             for (int n = 0; n < number.Count; n++)
             {
-                if (favNum > 10)
+                if (!number.Contains(favNum))
                 {
-                    Console.WriteLine("Try a number between 1 and 10");
-                    favNum = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("The string you choose is not in the list.");
+                    break;
+                }
+                if (favNum == number[n])
+                {
+                    Console.WriteLine(n);
+                    break;
+                    
                 }
 
             }
-            Console.WriteLine(favNum);
-
+ 
             Console.ReadLine();
 
-
-            String[] names = { "Jeremy", "Jeremy", "Skyla", "Sloan" };
-
-            foreach (string nick in names)
+            // Part 5 
+            List<string> names = new List<string> { "Jeremy", "Jeremy", "Skyla", "Sloan" };
+            Console.WriteLine("Enter a name to search");
+            string userInput2 = Console.ReadLine();
+            for (int i = 0; i < names.Count; i++)
             {
-                Console.WriteLine(nick);
+                if (!names.Contains(userInput2))
+                {
+                    Console.WriteLine("The string you choose is not in the list.");
+                    break;
+                }
+                if (userInput2 == names[i])
+                {
+                    Console.WriteLine(i);
+                    
+
+                }
             }
             Console.ReadLine();
-
-            
+            // Part 6 
+            List<string> dupeList = new List<string>();
+            foreach (string nick in names)
+            {
+                if (dupeList.Contains(nick))
+                {
+                    Console.WriteLine(nick + " is a duplicate");
+                }
+                else
+                {
+                    Console.WriteLine(nick + " has not been seen yet");
+                    dupeList.Add(nick);
+                }
+                
+            }
+            Console.ReadLine();
 
         }
     }
