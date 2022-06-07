30add45699c5ed4331054f4d675a492d0041fae9
Jeremy Sonnenschein <100240007+JeremySunshine@users.noreply.github.com>
2022-06-07 11:23:09 -0400

    methodSubmisson

---
+++ Projects/methodSubmission/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace methodSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate class
+            methods mathOpp = new methods();
+
+           
+
+            // asking user for num1
+            Console.WriteLine("Plese input a number: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            //asking user for num2
+            Console.WriteLine("Plese input a second number if you would like: ");
+            int userInput2= Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(mathOpp.parameters(userInput2));
+            Console.WriteLine(mathOpp.parameters(userInput2));
+
+
+
+        }
+    }
+}
---
+++ Projects/methodSubmission/methods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace methodSubmission
+{
+    class methods
+    {
+        // method that takes in two integers as parameters
+
+        public int parameters(int x, int y)
+        {
+            int z = x + y;
+            return z;
+
+        }
+    }
+}
