271e21802b5247f865121458171183ae5e868272
Jeremy Sonnenschein <100240007+JeremySunshine@users.noreply.github.com>
2022-06-07 10:23:55 -0400

    mainMethod Assignment

---
+++ Projects/mainMethodAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace mainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //insatansiating mathMethods 
+
+            mathMethods mathObject = new mathMethods();
+
+            // creating integer to pass through
+
+            int num1 = 10;
+            Console.WriteLine(mathObject.method1(num1));
+
+            // creating second integer
+
+            decimal num2 = 987;
+            Console.WriteLine(mathObject.method2(Convert.ToInt32(num2)));
+
+            // creating string to integer
+
+            string number = " 15 ";
+            Console.WriteLine(mathObject.method3(Convert.ToString(number)));
+        }
+    }
+}
---
+++ Projects/mainMethodAssignment/mathMethods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mainMethodAssignment
+{
+    class mathMethods
+    {
+        // Creating first math operatiion
+        public int method1(int number)
+        {
+            return number + 10;
+        }
+
+        // Creating second math operation
+
+        public int method2(int number2)
+        {
+            return number2 + 0;
+        }
+
+        public string method3(string number3)
+        {
+            return number3 + 15;
+        }
+    }
+}
