59e2310f2435085d2397ff7c3deae209631966f2
Jeremy Sonnenschein <100240007+JeremySunshine@users.noreply.github.com>
2022-06-07 16:50:38 -0400

    ClassSubmission update

--- Projects/classSubmissonAssignment/division.cs
+++ Projects/classSubmissonAssignment/division.cs
@@ -6,5 +6,19 @@ namespace classSubmissonAssignment
 {
     class division
     {
+        // creating method
+        public void myMethod(int number )
+        {
+            int z = number / 2;
+            Console.WriteLine(z);
+        }
+
+        //method overloading and also using out parameter
+
+        public void myMethod(out int number)
+        {
+            number = 22;
+
+        }
     }
 }
---
+++ Projects/classSubmissonAssignment/myStaticclass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace classSubmissonAssignment
+{
+    public static class  myStaticclass
+    {
+        public static void myStaticMehtod()
+        {
+            Console.WriteLine("this is from my static method");
+        }
+    }
+}
