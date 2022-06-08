cfcfbd8f5638fd40a489f9f0eb4517e0e7723b67
Jeremy Sonnenschein <100240007+JeremySunshine@users.noreply.github.com>
2022-06-08 08:13:03 -0400

    methodClass Submisson

---
+++ Projects/methodClassAssignment/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace methodClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate the class 
+            math mathOpp = new math();
+
+            mathOpp.Multiply();
+            
+        }
+    }
+}
---
+++ Projects/methodClassAssignment/math.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace methodClassAssignment
+{
+    class math
+    {
+        // Create a void method that takes in two integers as parameters
+        //Have the method do a math opp on 1st int and displays 2nd int to screen
+
+
+        public void Multiply(int x = 5, int y = 8)
+        {
+            int z = x * 5;
+            Console.WriteLine(y);
+        }
+    }
+}
