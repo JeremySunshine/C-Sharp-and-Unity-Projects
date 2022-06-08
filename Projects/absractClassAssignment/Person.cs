626730d8e34af8b222ad1f6957744d0be430477e
Jeremy Sonnenschein <100240007+JeremySunshine@users.noreply.github.com>
2022-06-08 13:13:06 -0400

    abstractClass

---
+++ Projects/absractClassAssignment/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace absractClassAssignment
+{
+    public abstract class Person
+    {
+
+        public string fName { get; set; }
+
+        public string lName { get; set; }
+
+        public abstract void SayName();
+        
+        
+
+    }
+}
---
+++ Projects/absractClassAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace absractClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee ID = new Employee();
+            ID.fName = "Sample ";
+            ID.lName = "Student";
+            ID.SayName();
+        }
+    }
+}
