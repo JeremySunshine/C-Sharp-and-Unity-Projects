94cb041534e81451f3aa2f6df87b66c3ba6c74d6
Jeremy Sonnenschein <100240007+JeremySunshine@users.noreply.github.com>
2022-06-16 11:03:48 -0400

    enumAssignment

---
+++ Projects/enumAssignment/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enumAssignment
+{
+    class Program
+    {
+        enum DayofTheWeek
+        {
+            Monday = 1,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Asking user to enter a day of the week
+                Console.WriteLine("Please enter the current day of the week 1-7?");
+                string userDay = Console.ReadLine();
+
+                DayofTheWeek day = (DayofTheWeek)Convert.ToInt32(userDay);
+
+                Console.WriteLine(day);
+                Console.ReadLine();
+            }
+
+            catch
+            {
+                Console.WriteLine("Please use the numbers 1- 7");
+            }
+            
+        }
+    }
+}
