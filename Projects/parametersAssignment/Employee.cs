ca05ca0aa3739bbf8885904322a20eec48004c6d
Jeremy Sonnenschein <100240007+JeremySunshine@users.noreply.github.com>
2022-06-16 08:28:26 -0400

    parametersAssignment

---
+++ Projects/parametersAssignment/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace parametersAssignment
+{
+    //gave employee class a generic type
+    public class Employee<T>
+    {
+        // added property called "things" and made generic list
+        public List<T> things { get; set; } 
+    }
+    
+
+}
---
+++ Projects/parametersAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace parametersAssignment
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employee = new Employee<string> { things = new List<string> { "Jeremy", "Skyla", "Sloan" } };
+            Employee<int> employeeInt = new Employee<int> { things = new List<int> { 28, 25, 2 } };
+            foreach (string emp in employee.things)
+            {
+                Console.WriteLine(emp);
+            }
+            
+        }
+    }
+}
