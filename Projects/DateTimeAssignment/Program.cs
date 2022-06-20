df58adcdea0ce6233a12dbb8c050a6c6cf0d45db
Jeremy Sonnenschein <100240007+JeremySunshine@users.noreply.github.com>
2022-06-20 11:35:28 -0400

    DateTime Assignment

---
+++ Projects/DateTimeAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DateTimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //prints current time to console
+            Console.WriteLine(DateTime.Now);
+
+            // ask user for input 
+            Console.WriteLine("Please input a number");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            DateTime timeNow = DateTime.Now;
+            DateTime future = timeNow.AddHours(userInput);
+            
+
+            //prints to console exact time in x hours
+            Console.WriteLine("In " + userInput + " it will be " + future);
+            
+            
+
+        }
+    }
+}
