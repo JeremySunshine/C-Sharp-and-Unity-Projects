821e261bd8558d527e5abb778ad6e3def8845b0f
Jeremy Sonnenschein <100240007+JeremySunshine@users.noreply.github.com>
2022-06-21 14:20:08 -0400

    fixed errors

--- Projects/lambdaAssignment/Program.cs
+++ Projects/lambdaAssignment/Program.cs
@@ -8,41 +8,44 @@ namespace lambdaAssignment
 {
     class Program
     {
-        public class Employee
+
+        static void Main(string[] args)
         {
-            private string name;
-            private int age;
-            private string designation;
-
-            // constructor
-            public  Employee(string name, int age, string designation)
+            List<Employee> employees = new List<Employee>
             {
-                this.name = name;
-                this.age = age;
-                this.designation = designation;
-            }
+                new Employee { firstName = "Joe", lastName = "Dirt", ID = 1},
+                new Employee { firstName = "Joe", lastName = "Swanson", ID = 2},
+                new Employee { firstName = "Bob", lastName = "Belcher", ID = 3},
+                new Employee { firstName = "Jimmy", lastName = "Pesto", ID = 4},
+                new Employee { firstName = "Sterling", lastName = "Archer", ID = 5},
+                new Employee { firstName = "Cyril", lastName = "Figgus", ID = 6},
+                new Employee { firstName = "Jeremy", lastName = "Sonnenschein", ID = 7},
+                new Employee { firstName = "Eric", lastName = "Cartman", ID = 8},
+                new Employee { firstName = "Stan", lastName = "Marsh", ID = 9},
+                new Employee { firstName = "Kenny", lastName = "McCormick", ID = 10}
+            };
 
-
-
-
-            static void Main(string[] args)
+            List<Employee> Joe1 = new List<Employee>();
+            
+            
+            foreach (Employee emp in employees)
             {
-                List<Employee> employee = new List<Employee>();
-                employee.Add("Joe", "Dirt", 1);
-                employee.Add("Joe", "Swanson", 2);
-                employee.Add("Peter", "Griffin", 3);
-                employee.Add("Stan", "Smith", 4);
-                employee.Add("Bob", "Burger", 5);
-                employee.Add("Jimmy", "Pesto", 6);
-                employee.Add("Sterling", "Archer", 7);
-                employee.Add("Cyril", "Figgus", 8);
-                employee.Add("Jeremy", "Sonnenschein", 9);
-                employee.Add("Joe", "Dirt", 10);
+                if (emp.firstName == "Joe")
+                {
+                    Joe1.Add(emp);
+                }
+            }
 
-
+            foreach (Employee emp in Joe1)
+            {
+                Console.WriteLine("{0} {1} {2}", emp.firstName, emp.lastName, emp.ID);
 
             }
-            }
+            List<Employee> Joe2 = employees.Where(x => x.firstName == "Joe").ToList();
+
+            List<Employee> empID = employees.Where(x => x.ID > 5).ToList();
         }
     }
+    
+    
 }
---
+++ Projects/lambdaAssignment/employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lambdaAssignment
+{
+    public class Employee
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int ID { get; set; }
+        
+
+    }
+}
